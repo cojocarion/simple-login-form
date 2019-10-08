f22d0a72902fee8ab01f037aac51f3e0222d812a
C0J <cojocari.1998@gmail.com>
2019-10-08 11:25:25 +0300

    created log in form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Button, FormGroup, FormControl } from "react-bootstrap";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+  }
+
+
+  
+  render() {
+    return (
+      <div className="Login">
+
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <h1>Welcome</h1>
+          </div>
+          <FormGroup controlId="email" >
+
+            <FormControl
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+              placeholder="email"
+            />
+          </FormGroup>
+          <FormGroup controlId="password" >
+
+            <FormControl
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+              placeholder="password"
+            />
+          </FormGroup>
+          <Button
+            block
+
+           
+            type="submit"
+            className="submit-button"
+          >
+            Login
+          </Button>
+        </form>
+      </div>
+    );
+  }
 }
-
-export default App;
